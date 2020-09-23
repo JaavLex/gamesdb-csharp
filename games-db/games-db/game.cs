0ddde770bb05ebaf7c9a5245459b3fbaabf0c52a
TacticsCH <alexandre.javet@hotmail.com>
2020-09-23 14:09:17 +0200

    [ADD] Game class + Add button
    - Added a game class
    - Added an "Add" button that I forgot in the last commit

---
+++ games-db/games-db/game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace games_db
+{
+    class game
+    {
+        public int _id { get; set; }
+        public string _name { get; set; }
+        public string _type { get; set; }
+        public string _platform { get; set; }
+        public DateTime _gamedate { get; set; }
+        public string _editor { get; set; }
+        public game()
+        {
+        }
+
+        public game(int ID, string Name, string Type, string Platform, DateTime GameDate, string Editor)
+        {
+            _id = ID;
+            _name = Name;
+            _type = Type;
+            _platform = Platform;
+            _gamedate = GameDate;
+            _editor = Editor;
+        }
+    }
+}
